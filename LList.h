9fa4754a41e336e44fb4594dab1ef014b7e125a9
nataliecrawford <crawfon@email.sc.edu>
2022-11-30 13:49:30 -0500

    did the llist.h file

--- LList.h
+++ LList.h
@@ -8,8 +8,31 @@ class LList
     private:
     /* data */
     public:
-        LList(/* args */);
+        LList();
+        LList(int[] anArray, int size);
+        LList(const LList &);
         ~LList();
+
+        const LList operator+(const LList &rhs) const;
+        const LList operator=(const LList &rhs);
+
+        void insert(const int index, const int &value);
+        int remove(const int index);
+        bool contains(const int &value) const;
+        int get(const int index);
+        int indexOf(const int &value) const;
+        
+        bool isEmpty() const;
+        int size() const;
+        void clear();
+
+        const LList operator<<() const;
+        bool operator==(const LList &rhs) const;
+
+        int operator[](int index) const;
+        int & operator[](int index);
+
+
 };
 
 #endif //LLIST.H
